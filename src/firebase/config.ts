a2522cc4ae163c0b22e816f1206b26dace97211c
LIkit Sea-Ngow (Song) <iamsongyee@gmail.com>
2025-11-07 14:57:40 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5216740698-cadc5",
+  "appId": "1:921614503967:web:8d01cc6b6603b77a2f052a",
+  "apiKey": "AIzaSyBUTa_chfrPSf2cun8pkGOeYy_8tA2TYl4",
+  "authDomain": "studio-5216740698-cadc5.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "921614503967"
+};
