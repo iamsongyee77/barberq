c3c11d3281c6461d763d7aace253d0fe5c6771ae
LIkit Sea-Ngow (Song) <iamsongyee@gmail.com>
2025-11-18 11:53:24 +0000

    why menu(edit, delete) of user disaapear

--- src/app/admin/users/page.tsx
+++ src/app/admin/users/page.tsx
@@ -157,7 +157,9 @@ export default function UsersPage() {
                     <TableCell><Skeleton className="h-5 w-24" /></TableCell>
                     <TableCell><Skeleton className="h-5 w-48" /></TableCell>
                     <TableCell><Skeleton className="h-6 w-20" /></TableCell>
-                    <TableCell><MoreHorizontal className="h-4 w-4" /></TableCell>
+                    <TableCell className="text-right">
+                      <Skeleton className="h-8 w-8 ml-auto" />
+                    </TableCell>
                   </TableRow>
                 ))}
               {!isLoading && allUsers.length === 0 && (
